81451f0affb492b59a5c433daa65a916af84db09
DujeV <dvidov01@fesb.hr>
2020-11-12 19:51:33 +0100

    adding style for NavBar and MenuIcon

---
+++ src/components/NavBar/Navigation/MenuIcon/MenuIcon.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./MenuIcon.css"
+import MenuIcon from "@material-ui/icons/Menu"
+const Menu_icon = () => (
+  <div className="MenuIcon">
+    <MenuIcon fontSize="large" />
+  </div>
+)
+
+export default Menu_icon
