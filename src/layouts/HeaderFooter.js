0f19e0c4fd83a5de77e5963501b803c26f97db3f
tblaze77 <toni.blazevic77@gmail.com>
2020-11-28 23:50:21 +0100

    defining footer structure

--- src/layouts/HeaderFooter.js
+++ src/layouts/HeaderFooter.js
@@ -1,10 +1,12 @@
 import React from "react"
 import NavigationHeader from "../modules/NavigationHeader/NavigationHeader"
+import Footer from "../components/Footer/Footer"
 
 const HeaderFooterLayout = ({ children }) => (
   <>
     <NavigationHeader />
     {children}
+    <Footer />
   </>
 )
 
