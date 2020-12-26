0ad4ef4b10ea32435fc7146fd34a974096ad46b6
DujeV <dvidov01@fesb.hr>
2020-12-26 18:03:46 +0100

    adding background image and counter css refator

--- src/components/TitleImage/TitleImage.js
+++ src/components/TitleImage/TitleImage.js
@@ -1,12 +1,31 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import "./TitleImage.css"
 import Counter from "./Counter"
+import BackgroundImage from "gatsby-background-image"
 
 const TitleImage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "Background.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1920, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   return (
-    <>
+    <BackgroundImage
+      Tag="section"
+      className="backgroundImage"
+      fluid={data.desktop.childImageSharp.fluid}
+      backgroundColor={`#040e18`}
+    >
       <Counter />
-    </>
+    </BackgroundImage>
   )
 }
 
