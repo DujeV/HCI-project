16001053eb0c498c25ffbe11f2f91b4c0babd3cc
tblaze77 <toni.blazevic77@gmail.com>
2021-01-03 00:49:31 +0100

    adding sort functionality to teams page

--- src/components/TeamsInfo/TeamsInfo.js
+++ src/components/TeamsInfo/TeamsInfo.js
@@ -1,16 +1,65 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import BackgroundGradient from "../BackgroundGradient"
 import { teams } from "./TeamsData/TeamsData.js"
 import "./TeamsInfo.css"
 
 const TeamsInfo = () => {
-  {
+  
+    const [data, setData] = useState([]);
+    const [sortType, setSortType] = useState('A-Z');
+
+    useEffect(() => {
+
+      setData( [...teams].sort(compare));
+  }, [sortType]); 
+
+  function compare( a, b ) {
+
+    if(sortType === "A-Z")
+    {
+      if ( a.nation < b.nation ){
+        return -1;
+      }
+       else if ( a.nation > b.nation ){
+        return 1;
+      }
+    } 
+    
+     else if (sortType === "Z-A")
+    {
+      if ( a.nation < b.nation ){
+        return 1;
+      }
+       else if ( a.nation > b.nation ){
+        return -1;
+      }
+    }
+    
+    else if(sortType === "Euros Played")
+    {
+      if ( a.eurosPlayed < b.eurosPlayed ){
+        return 1;
+      }
+       else if( a.eurosPlayed > b.eurosPlayed ){
+        return -1;
+      }
+    } 
+  }
+
     return (
       <BackgroundGradient>
         <div className="mainContainer">
           <h1>Teams</h1>
+
+          <label htmlFor="select">Sort by</label>
+          <select onChange={(e) => setSortType(e.target.value)}>
+            <option value="A-Z">A-Z</option>
+            <option value="Z-A">Z-A</option>
+            <option value="Euros Played">Euros Played</option>
+          </select>
+
           <div className="cards">
-            {teams.map(
+            {data.map(
               ({ flag, nation, eurosPlayed, bestPlace, bestPlayer }) => (
                 <div className="card">
                   <div className="nationContainer">
@@ -36,7 +85,7 @@ const TeamsInfo = () => {
         </div>
       </BackgroundGradient>
     )
-  }
+  
 }
 
 export default TeamsInfo
