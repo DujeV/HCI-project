ac1cb2c883d08bd47fbb32efa6948a14425ad515
tblaze77 <toni.blazevic77@gmail.com>
2021-01-13 18:13:03 +0100

    adding search funcionality to teams component

--- src/components/TeamsInfo/TeamsInfo.js
+++ src/components/TeamsInfo/TeamsInfo.js
@@ -1,91 +1,114 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import BackgroundGradient from "../BackgroundGradient"
 import { teams } from "./TeamsData/TeamsData.js"
 import "./TeamsInfo.css"
+//SortBy
 
 const TeamsInfo = () => {
-  
-    const [data, setData] = useState([]);
-    const [sortType, setSortType] = useState('A-Z');
+  const [data, setData] = useState([])
+  const [sortType, setSortType] = useState("A-Z")
+  const [search, setSearch] = useState("")
 
-    useEffect(() => {
+  useEffect(() => {
+    let results = []
 
-      setData( [...teams].sort(compare));
-  }, [sortType]); 
+    //
+    if (search.length < 1) {
+      setData([...teams].sort(compare))
+    }
+    //
+    else if (search.length > 0) {
+      results = data.filter(team =>
+        team.nation.toLowerCase().match(search.toLowerCase())
+      )
+      setData(results.sort(compare))
+    }
+  }, [sortType, search])
 
-  function compare( a, b ) {
-
-    if(sortType === "A-Z")
-    {
-      if ( a.nation < b.nation ){
-        return -1;
+  function compare(a, b) {
+    if (sortType === "A-Z") {
+      if (a.nation < b.nation) {
+        return -1
+      } else if (a.nation > b.nation) {
+        return 1
       }
-       else if ( a.nation > b.nation ){
-        return 1;
+    } else if (sortType === "Z-A") {
+      if (a.nation < b.nation) {
+        return 1
+      } else if (a.nation > b.nation) {
+        return -1
       }
-    } 
-    
-     else if (sortType === "Z-A")
-    {
-      if ( a.nation < b.nation ){
-        return 1;
-      }
-       else if ( a.nation > b.nation ){
-        return -1;
+    } else if (sortType === "Euros Played") {
+      if (a.eurosPlayed < b.eurosPlayed) {
+        return 1
+      } else if (a.eurosPlayed > b.eurosPlayed) {
+        return -1
       }
     }
-    
-    else if(sortType === "Euros Played")
-    {
-      if ( a.eurosPlayed < b.eurosPlayed ){
-        return 1;
-      }
-       else if( a.eurosPlayed > b.eurosPlayed ){
-        return -1;
-      }
-    } 
   }
 
-    return (
-      <BackgroundGradient>
-        <div className="mainContainer">
-          <h1>Teams</h1>
+  const handleChange = event => {
+    event.preventDefault()
+    setSearch(event.target.value)
+  }
 
+  return (
+    <BackgroundGradient>
+      <div className="mainContainer">
+        <h1>Teams</h1>
+
+        <div className="dropdownBox">
           <label htmlFor="select">Sort by</label>
-          <select onChange={(e) => setSortType(e.target.value)}>
-            <option value="A-Z">A-Z</option>
-            <option value="Z-A">Z-A</option>
-            <option value="Euros Played">Euros Played</option>
-          </select>
-
-          <div className="cards">
-            {data.map(
-              ({ flag, nation, eurosPlayed, bestPlace, bestPlayer }) => (
-                <div className="card">
-                  <div className="nationContainer">
-                    <img src={flag.big} alt={flag.alt} />
-                    <p>{nation}</p>
-                  </div>
-                  <div className="infoContainer">
-                    <div className="infoTitles">
-                      <p>Euros played</p>
-                      <p>Best place</p>
-                      <p>Best player</p>
-                    </div>
-                    <div className="infoContent">
-                      <p>{eurosPlayed}</p>
-                      <p>{bestPlace}</p>
-                      <p>{bestPlayer}</p>
-                    </div>
-                  </div>
-                </div>
-              )
-            )}
+          <div className="selectedDropDown">
+            <select onChange={e => setSortType(e.target.value)}>
+              <option value="A-Z">A-Z</option>
+              <option value="Z-A">Z-A</option>
+              <option value="Euros Played">Euros Played</option>
+            </select>
           </div>
         </div>
-      </BackgroundGradient>
-    )
-  
+
+        <div className="searchContainer">
+          <label htmlFor="input">Search teams:</label>
+          <div className="inputField">
+            <input
+              type="text"
+              placeholder="Teams"
+              value={search}
+              onChange={handleChange}
+            ></input>
+          </div>
+        </div>
+
+        <div
+          className={data.length <= 1 ? "cards activeOne" : "cards activeMore"}
+        >
+          {data.map(
+            ({ flag, nation, eurosPlayed, bestPlace, bestPlayer, id }) => (
+              <div className="card" key={id}>
+                <div className="nationContainer">
+                  <img src={flag.big} alt={flag.alt} />
+                  <p>{nation}</p>
+                </div>
+                <div className="infoContainer">
+                  <div className="infoTitles">
+                    <p>Euros played</p>
+                    <p>Best place</p>
+                    <p>Best player</p>
+                  </div>
+                  <div className="infoContent">
+                    <p>{eurosPlayed}</p>
+                    <p>{bestPlace}</p>
+                    <p>{bestPlayer}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </BackgroundGradient>
+  )
 }
 
 export default TeamsInfo
