57cce5a690c4a5bf4934bca79a0f292ee57e0a67
tblaze77 <toni.blazevic77@gmail.com>
2021-01-15 22:04:34 +0100

    adding blog data

---
+++ src/components/Blogs/BlogData.js
@@ -0,0 +1,65 @@
+export const blogs = [
+    {
+      title: "Latest ticket information",
+      author: "Toni Blažević",
+      time: "Thu, 14th January 2021",
+      text:
+        "The tournament will take place from 11 June to 11 July 2021. More ticket information will be available in 2021, including for matches involving Hungary, North Macedonia, Scotland and Slovakia.",
+    },
+    {
+      title: "Predictions for top scorer",
+      author: "Duje Vidović",
+      time: "Thu, 8th January 2021",
+      text:
+        "Englandâ€™s Harry Kane has finished as Alipay Top Scorer having struck 12 goals in the European Qualifiers for UEFA EURO 2021.",
+    },
+    {
+      title: "Will Portugal be able to defend their title?",
+      author: "Toni Blažević",
+      time: "Thu, 14th January 2021",
+      text:
+        "According to Bild, Portugal will have a good chance to defend their title they have won in 2016 Euro. With Cristiano Ronaldo as their top player, they are on mission make their nation proud.",
+    },
+    {
+      title: "Latest ticket information",
+      author: "Duje Vidović",
+      time: "Thu, 8th January 2021",
+      text:
+        "The tournament will take place from 11 June to 11 July 2021. More ticket information will be available in 2021, including for matches involving Hungary, North Macedonia, Scotland and Slovakia.",
+    },
+    {
+      title: "Latest ticket information",
+      author: "Toni Blažević",
+      time: "Thu, 14th January 2021",
+      text:
+        "The tournament will take place from 11 June to 11 July 2021. More ticket information will be available in 2021, including for matches involving Hungary, North Macedonia, Scotland and Slovakia.",
+    },
+    {
+      title: "Latest ticket information",
+      author: "Duje Vidović",
+      time: "Thu, 8th January 2021",
+      text:
+        "The tournament will take place from 11 June to 11 July 2021. More ticket information will be available in 2021, including for matches involving Hungary, North Macedonia, Scotland and Slovakia.",
+    },
+  ]
+  
+  export const tags = [
+    {
+      name: "all",
+    },
+    {
+      name: "prize",
+    },
+    {
+      name: "players",
+    },
+    {
+      name: "trophy",
+    },
+    {
+      name: "tickets",
+    },
+    {
+      name: "nations",
+    },
+  ]
