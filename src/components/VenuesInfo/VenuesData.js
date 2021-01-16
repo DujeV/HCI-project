bad2b36c58d57aa0b088b9acaaa8e00837e76250
tblaze77 <toni.blazevic77@gmail.com>
2021-01-16 19:19:52 +0100

    adding venues data

---
+++ src/components/VenuesInfo/VenuesData.js
@@ -0,0 +1,32 @@
+export const venues = [
+    {
+      name: "Wembley stadium, London, UK",
+      capacity: 90000,
+      groupsPlaying: "A,D",
+      info:
+        "Second largest stadium in Europe, right behind Camp Nou. Also the most expensive football stadium ever.",
+      image: "wembley.jpg",
+    },
+    {
+      name: "San Siro, Milano, Italy",
+      capacity: 80000,
+      groupsPlaying: "A,C",
+      info:
+        "Also called â€œGiuseppe Meazzaâ€ is a home to both AC Milan and Inter FC football clubs.",
+      image: "san_siro.jpg",
+    },
+    {
+      name: "Allianz Arena, Munchen, Germany",
+      capacity: 70000,
+      groupsPlaying: "B,D",
+      info: "Home to Bayern Munchen FC. World Cup 2006. was played there.",
+      image: "allianz_arena.jpg",
+    },
+    {
+      name: "Wembley stadium, London, UK",
+      capacity: 60000,
+      groupsPlaying: "B,C",
+      info: "Winner (2016)",
+      image: "baku.jpg",
+    },
+  ]
