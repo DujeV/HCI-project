d9546e603e2dd27f83a3ce7095eaa6f7e64db644
tblaze77 <toni.blazevic77@gmail.com>
2021-01-16 19:21:42 +0100

    adding venues component

---
+++ src/components/VenuesInfo/VenuesInfo.js
@@ -0,0 +1,70 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundGradient from "../BackgroundGradient"
+import Img from "gatsby-image"
+import "./VenuesInfo.css"
+import { venues } from "./VenuesData"
+const VenuesInfo = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allFile(filter: { relativeDirectory: { eq: "venues" } }) {
+        edges {
+          node {
+            base
+            childImageSharp {
+              fluid(maxWidth: 1920, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <BackgroundGradient>
+      <div className="mainContainer">
+        <h1>Venues and host cities</h1>
+        <div className="venuesContainer">
+          {venues.map((venue, index) => (
+            <div className="venue" key={index}>
+              {data.allFile.edges.map(({ node }, index) => {
+                if (node.base === venue.image) {
+                  return (
+                    <Img
+                      fluid={node.childImageSharp.fluid}
+                      Tag="section"
+                      className="venueTumbnail"
+                      key={index}
+                    />
+                  )
+                }
+              })}
+
+              <div className="venueContent">
+                <p className="venueName">{venue.name}</p>
+                <div className="venueInfo">
+                  <div className="venueBox">
+                    <span>Capacity</span>
+                    <p>{venue.capacity}</p>
+                  </div>
+                  <div className="venueBox">
+                    <span>Groups playing</span>
+                    <p> {venue.groupsPlaying}</p>
+                  </div>
+                  <div className="venueBox">
+                    <span>Info</span>
+                    <p>{venue.info}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </BackgroundGradient>
+  )
+}
+
+export default VenuesInfo
