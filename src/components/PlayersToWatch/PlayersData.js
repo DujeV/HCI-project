234c460dfdb15e4650d042f6ffc4749f28615d4b
DujeV <dvidov01@fesb.hr>
2021-01-16 19:00:54 +0100

    adding players data

---
+++ src/components/PlayersToWatch/PlayersData.js
@@ -0,0 +1,29 @@
+export const players = [
+  {
+    name: "Cristiano Ronaldo",
+    age: 35,
+    nationality: "Portuguese",
+    euroRecord: "Winner (2016)",
+    matchesPlayed: 21,
+    goalsScored: 9,
+    image: "cristiano_ronaldo.jpg",
+  },
+  {
+    name: "Luka Modrić",
+    age: 35,
+    nationality: "Croatian",
+    euroRecord: "QF (2008)",
+    matchesPlayed: 9,
+    goalsScored: 2,
+    image: "luka_modric.jpg",
+  },
+  {
+    name: "Robert Lewandowski",
+    age: 32,
+    nationality: "Pole",
+    euroRecord: "Group stage (2012)",
+    matchesPlayed: 8,
+    goalsScored: 2,
+    image: "robert_lewandowski.jpg",
+  },
+]
