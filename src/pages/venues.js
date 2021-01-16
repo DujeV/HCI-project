d4b940e51d99ac20f3300d0196dc4272ed2c9d7a
tblaze77 <toni.blazevic77@gmail.com>
2021-01-16 19:24:13 +0100

    adding style to venues

--- src/pages/venues.js
+++ src/pages/venues.js
@@ -1,9 +1,11 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/HeaderFooter"
 import Quote from "../components/Quote/Quote"
+import VenuesInfo from "../components/VenuesInfo/VenuesInfo"
 
 const Venues = () => (
   <HeaderFooterLayout activeTab="Venues">
+    <VenuesInfo />
     <Quote
       quote="Everyone is still talking about that goal in the 1988 final – I think it was a bit lucky."
       author="Rafael van der Vaart"
