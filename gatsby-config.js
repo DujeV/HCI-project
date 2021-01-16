4f2161c1054f5f28a5987b6155a22223c7e2fca9
tblaze77 <toni.blazevic77@gmail.com>
2021-01-16 22:41:24 +0100

    setting up contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -35,5 +37,13 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_KEY,
+        downloadLocal: true,
+      }
+    }
   ],
 }
