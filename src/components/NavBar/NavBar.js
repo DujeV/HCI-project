6e8e13422b297966b43e3dabb06635311fc05cd6
tblaze77 <toni.blazevic77@gmail.com>
2021-01-18 19:10:48 +0100

    adding login functionality

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Link } from "gatsby"
+import { Link, navigate} from "gatsby"
 
 import MenuIcon from "@material-ui/icons/Menu"
 import CloseIcon from "@material-ui/icons/Close"
@@ -8,12 +8,26 @@ import AccountCircleIcon from "@material-ui/icons/AccountCircle"
 import "./NavBar.css"
 import LogoSVG from "../../svgs/logo.svg"
 import { navs } from "../../constants/const"
+import Login from "../Login/Login";
 
 function Navigation({ activeTab }) {
+  const [user,setUser] = useState(localStorage.getItem('loggedIn'));
   const [click, setClick] = useState(false)
+  const [isModalOpen, setIsModalOpen]=useState(false);
 
   const handleClick = () => setClick(!click)
   const closeMobileMenu = () => setClick(false)
+
+  const closeModal = () => setIsModalOpen(false)
+
+  const Logout = () => {
+    localStorage.clear();
+    setUser(null);
+    return navigate('/');
+  }
+  const UpdatingUser = () => {
+    setUser(localStorage.getItem('loggedIn'));
+  }
 
   return (
     <div className="navbarContainer">
@@ -36,10 +50,11 @@ function Navigation({ activeTab }) {
             {tab}
           </Link>
         ))}
-        <button className="loginButton">
+        <button className="loginButton" onClick={() => setIsModalOpen(!isModalOpen)}>
           <AccountCircleIcon className="userIcon" />
           <span>Log In</span>
         </button>
+        <Login isModalOpen={isModalOpen} closeModal={closeModal} UpdatingUser={UpdatingUser}/>
       </li>
     </div>
   )
